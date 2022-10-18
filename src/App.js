add7401ae0e7c81a990a0196de93945b1ef7d15c
Anthony_Dellavecchia <dev@anthonydellavecchia.com>
2022-10-18 00:16:17 -0700

    feat(init): djangocon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./images/twilioQuest.png";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button
+                    onClick={() => {
+                        window.parent.postMessage({ hide: true }, "*");
+                    }}
+                >
+                    X
+                </button>
+                <img src={logo} className="App-logo" alt="logo" />
+                <p id="welcome">
+                    Welcome to <strong>DjangoCon</strong>!
+                </p>
+                <p id="game">Play our game to win Swag!</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
